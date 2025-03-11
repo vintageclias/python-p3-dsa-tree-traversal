b26beda7f56341582323af7d4dd23a4d426ab955
vintageclias <vintageclias@gmail.com>
2025-03-11 09:26:02 -0700

    tree-traveral

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,13 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop()
+      if node['id'] == id:
+        return node
+      
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    else:
+      return None
